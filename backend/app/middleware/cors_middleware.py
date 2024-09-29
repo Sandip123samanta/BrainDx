fe2d0010ccb47454a2af6105ebc3d4b33c6acac0
Sandip123samanta <111598408+Sandip123samanta@users.noreply.github.com>
2024-09-29 10:06:57 +0530

    Origins added

--- backend/app/middleware/cors_middleware.py
+++ backend/app/middleware/cors_middleware.py
@@ -1,10 +1,9 @@
 from fastapi.middleware.cors import CORSMiddleware
 
 def add_cors_middleware(app):
-
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],
+        allow_origins=["http://localhost:5173", "https://brain-dx.vercel.app"], 
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"]
